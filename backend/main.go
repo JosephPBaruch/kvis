f3690e1ff548504a13c96aec003a71b9a4ffe43f
Joseph Baruch <josephbaruch48@gmail.com>
2025-01-27 18:15:04 -0800

    Correcting network port conflict

--- backend/main.go
+++ backend/main.go
@@ -5,10 +5,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("BACKEND_PORT")
+	if port == "" {
+		port = "8082" // Default port if not set
+	}
+
 	routes.Routes()
-	fmt.Println("Server is running on port 8082...")
-	log.Fatal(http.ListenAndServe(":8082", nil))
+	fmt.Printf("Server is running on port %s...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }

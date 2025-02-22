902092f948da0c31b191bab08fa82129382d5fd3
Joseph Baruch <josephbaruch48@gmail.com>
2025-02-21 19:54:54 -0800

    correcting

--- dummy/serverPipeline.sh
+++ dummy/serverPipeline.sh
@@ -24,25 +24,25 @@ cleanup() {
         docker rmi $CLIENT_IMAGE
     fi
 
-    # if [ "$(docker images -q $BACKEND_IMAGE 2> /dev/null)" ]; then
-    #     docker rmi $BACKEND_IMAGE
-    # fi
+    if [ "$(docker images -q $BACKEND_IMAGE 2> /dev/null)" ]; then
+        docker rmi $BACKEND_IMAGE
+    fi
 
     if [ "$(docker images -q $REGISTRY/$CLIENT_IMAGE 2> /dev/null)" ]; then
         docker rmi $REGISTRY/$CLIENT_IMAGE
     fi
 
-    # if [ "$(docker images -q $REGISTRY/$BACKEND_IMAGE 2> /dev/null)" ]; then
-    #     docker rmi $REGISTRY/$BACKEND_IMAGE
-    # fi
+    if [ "$(docker images -q $REGISTRY/$BACKEND_IMAGE 2> /dev/null)" ]; then
+        docker rmi $REGISTRY/$BACKEND_IMAGE
+    fi
 
     # Delete all Kubernetes resources
     kubectl delete -f ingress.yaml
     kubectl delete -f server_manifests/client_deployment.yaml
     kubectl delete -f client_service.yaml
-    # kubectl delete -f server_manifests/backend_deployment.yaml
-    # kubectl delete -f backend_service.yaml
-    # kubectl delete -f pvc.yaml
+    kubectl delete -f server_manifests/backend_deployment.yaml
+    kubectl delete -f backend_service.yaml
+    kubectl delete -f pvc.yaml
 }
 
 run() {
@@ -61,26 +61,26 @@ run() {
     fi
 
     # # Build and push the backend Docker image
-    # if [[ "$(docker images -q $BACKEND_IMAGE 2> /dev/null)" == "" ]]; then
-    #     echo "Building the backend Docker image..."
-    #     cd ../backend
-    #     docker build -t $BACKEND_IMAGE .
-    #     cd ../dummy
-    # fi
+    if [[ "$(docker images -q $BACKEND_IMAGE 2> /dev/null)" == "" ]]; then
+        echo "Building the backend Docker image..."
+        cd ../backend
+        docker build -t $BACKEND_IMAGE .
+        cd ../dummy
+    fi
 
     docker tag $CLIENT_IMAGE $REGISTRY/$CLIENT_IMAGE
-    # docker tag $BACKEND_IMAGE $REGISTRY/$BACKEND_IMAGE
+    docker tag $BACKEND_IMAGE $REGISTRY/$BACKEND_IMAGE
 
     docker push $REGISTRY/$CLIENT_IMAGE
-    # docker push $REGISTRY/$BACKEND_IMAGE
+    docker push $REGISTRY/$BACKEND_IMAGE
 
     # Apply Kubernetes manifests
     kubectl apply -f ingress.yaml
     kubectl apply -f server_manifests/client_deployment.yaml
     kubectl apply -f client_service.yaml
-    # kubectl apply -f server_manifests/backend_deployment.yaml
-    # kubectl apply -f backend_service.yaml
-    # kubectl apply -f pvc.yaml
+    kubectl apply -f server_manifests/backend_deployment.yaml
+    kubectl apply -f backend_service.yaml
+    kubectl apply -f pvc.yaml
 
     # ./curl_pod/pipeline.sh
 }

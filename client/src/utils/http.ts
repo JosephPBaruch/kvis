96f30201c8fe9f9d15e3503236a2f55f62e73f13
Joseph Baruch <josephbaruch48@gmail.com>
2025-02-23 12:44:12 -0800

    fix

--- client/src/utils/http.ts
+++ client/src/utils/http.ts
@@ -1,6 +1,6 @@
 import { ReturnPromiseList, ReturnPromiseDetails } from '../types/types';
 
-const baseURL = 'http://localhost:8090';
+const baseURL = 'http://localhost:8090/backend';
 
 const ListObjects = async (type: string | undefined): ReturnPromiseList => {
   const response = await fetch(`${baseURL}/${type}`, {

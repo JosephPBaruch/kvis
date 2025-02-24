634dbbb92cc5dfa9ccee8ecdda803216a9e904de
Joseph Baruch <josephbaruch48@gmail.com>
2025-02-23 20:06:45 -0800

    Correct pathing

--- dummy/cleanServer.sh
+++ dummy/cleanServer.sh
@@ -3,11 +3,11 @@
 
 echo "Deleting specific deployments..."
 
-kubectl delete -f /Users/joseph.baruch/REPO/personal/kvis/dummy/server_manifests/backend_deployment.yaml
-kubectl delete -f /Users/joseph.baruch/REPO/personal/kvis/dummy/server_manifests/client_deployment.yaml
-kubectl delete -f /Users/joseph.baruch/REPO/personal/kvis/dummy/ingress.yaml
-kubectl delete -f /Users/joseph.baruch/REPO/personal/kvis/dummy/client_service.yaml
-kubectl delete -f /Users/joseph.baruch/REPO/personal/kvis/dummy/backend_service.yaml
-kubectl delete -f /Users/joseph.baruch/REPO/personal/kvis/dummy/pvc.yaml
+kubectl delete -f ./server_manifests/backend_deployment.yaml
+kubectl delete -f ./server_manifests/client_deployment.yaml
+kubectl delete -f ./ingress.yaml
+kubectl delete -f ./client_service.yaml
+kubectl delete -f ./backend_service.yaml
+kubectl delete -f ./pvc.yaml
 
 echo "Specific deployments have been deleted."
